cfd434bf72e4fea6ce2e22f508b8c1513a8ef922
rndquu <rndquu@gmail.com>
2023-04-21 16:07:49 +0300

    feat: use dotenv

--- js/proposeTx.ts
+++ js/proposeTx.ts
@@ -8,23 +8,19 @@ import Safe from "@safe-global/safe-core-sdk";
 import { OperationType, SafeTransactionDataPartial } from "@safe-global/safe-core-sdk-types";
 import EthersAdapter from "@safe-global/safe-ethers-lib";
 import SafeServiceClient from "@safe-global/safe-service-client";
+import * as dotenv from 'dotenv';
 import { ethers } from "ethers";
 import DAI_ABI from "./DAI_abi.json";
 
-const config = {
-  CHAIN_ID: 5, // mainnet: 1, goerli: 5
-  RPC_URL: "https://goerli.infura.io/v3/0697ca1ac6d04ea7a86a146e53452fb9",
-  BOT_PRIVATE_KEY: "0xa3bfde12b832ab44f303ecb8acfb5042190f357ec7712a8597c29ba1e1a31708",
-  SAFE_ADDRESS: "0x839F2406464B98128c67c00dB9408F07bB9D4629",
-  TX_SERVICE_URL: "https://safe-transaction-goerli.safe.global/", // Check https://docs.safe.global/backend/available-services
-  DAI_ADDRESS: "0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844", // mainnet: 0x6B175474E89094C44Da98b954EedeAC495271d0F, goerli: 0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844
-  BOUNTY_HUNTER_ADDRESS: "0x9694227ef9516A6AB4FBF30039686E5f1AeF0b41",
-  BOUNTY_HUNTER_REWARD_ETH: 1,
-};
+dotenv.config();
+
+// variables depending on a bounty hunter wallet and reward
+const bountyHunterAddress = '0xB52e2e8ED4C4B57ddD41FA5b62e721b90e77A36b';
+const bountyHunterRewardEth = '0.0001';
 
 async function main() {
-  const provider = new ethers.providers.JsonRpcProvider(config.RPC_URL);
-  const signer = new ethers.Wallet(config.BOT_PRIVATE_KEY, provider);
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  const signer = new ethers.Wallet(String(process.env.BOT_PRIVATE_KEY), provider);
 
   // create EthAdapter instance
   const ethAdapter = new EthersAdapter({
@@ -35,29 +31,29 @@ async function main() {
   // create Safe instance
   const safe = await Safe.create({
     ethAdapter,
-    safeAddress: config.SAFE_ADDRESS,
+    safeAddress: String(process.env.SAFE_ADDRESS),
   });
 
   // Create Safe Service Client instance
   const service = new SafeServiceClient({
-    txServiceUrl: config.TX_SERVICE_URL,
+    txServiceUrl: String(process.env.SAFE_TX_SERVICE_URL),
     ethAdapter,
   });
 
   // create DAI contract instance
-  const daiContract = new ethers.Contract(config.DAI_ADDRESS, DAI_ABI);
+  const daiContract = new ethers.Contract(String(process.env.DAI_ADDRESS), DAI_ABI);
   const transferData = await daiContract.populateTransaction.transfer(
-    config.BOUNTY_HUNTER_ADDRESS,
-    ethers.utils.parseEther(String(config.BOUNTY_HUNTER_REWARD_ETH)).toString()
+    bountyHunterAddress,
+    ethers.utils.parseEther(bountyHunterRewardEth).toString()
   );
 
   // get next safe tx nonce (safe nonces are different from EOA nonces)
   // next nonce is the next nonce after the last queued (not yet executed) transaction
-  const nextNonce = await service.getNextNonce(config.SAFE_ADDRESS);
+  const nextNonce = await service.getNextNonce(String(process.env.SAFE_ADDRESS));
 
   // create transaction
   const safeTransactionData: SafeTransactionDataPartial = {
-    to: config.DAI_ADDRESS,
+    to: String(process.env.DAI_ADDRESS),
     value: "0", // in wei
     data: String(transferData.data),
     operation: OperationType.Call,
@@ -71,7 +67,7 @@ async function main() {
   const signature = await safe.signTransactionHash(safeTxHash);
 
   await service.proposeTransaction({
-    safeAddress: config.SAFE_ADDRESS,
+    safeAddress: String(process.env.SAFE_ADDRESS),
     safeTransactionData: safeTransaction.data,
     safeTxHash,
     senderAddress,
@@ -79,8 +75,8 @@ async function main() {
   });
 
   console.log(
-    `Transaction URL: https://app.safe.global/${config.CHAIN_ID === 5 ? "gor" : "eth"}:${config.SAFE_ADDRESS}/transactions/tx?id=multisig_${
-      config.SAFE_ADDRESS
+    `Transaction URL: https://app.safe.global/${process.env.CHAIN_ID && +process.env.CHAIN_ID === 1 ? "eth" : "matic"}:${process.env.SAFE_ADDRESS}/transactions/tx?id=multisig_${
+      process.env.SAFE_ADDRESS
     }_${safeTxHash}`
   );
 }
